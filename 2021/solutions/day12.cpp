1539dba69ada783a0308e24dd9c8627f10977bd4
Alexander Löhr <a.loehr@ymail.com>
2021-12-12 09:41:46 +0100

    optimized day 12

--- 2021/solutions/day12.cpp
+++ 2021/solutions/day12.cpp
@@ -3,46 +3,49 @@
 #include <vector>
 #include <map>
 #include <cassert>
-#include <set>
 
 #include "../aoc.hpp"
 
-// we have a small cave if the name is written in small letters
-bool small_cave(int node_id, std::map<int, std::string>& node_id_names) {
-    return node_id_names[node_id][0] >= 97;
+// checks if a number is already stored inside a vector
+bool is_in(const std::vector<int>& v, const int cmp) {
+    for (const int n : v) if (n == cmp) return true;
+
+    return false;
 }
 
-// counts recursively all pathes from a starting point to the end node
-// visited_twice == true -> p1
-// visited_twice == false -> p2
-int count_all_pathes(int cur_node,
-        std::map<std::string, int>& node_ids,
-        std::map<int, std::set<int>>& edges,
-        std::map<int, std::string>& node_id_names,
-        bool visited_twice = false,
-        std::set<int> visited = std::set<int>()
-    ) {
+// counts recursively all pathes from a starting point to the end node (DFS)
+// visited_twice == false allows one small cave to be visited twice (p2)
+int count_all_pathes(
+    const int cur_node,
+    const std::vector<std::vector<int>>& edges,
+    const std::vector<bool>& is_small_cave,
+    const int start,
+    const int end,
+    const bool visited_twice = false
+) {
+    static std::vector<int> visited;
 
-    // we are at the end, return 1 for having found one path to the end
-    if (cur_node == node_ids["end"]) return 1;
-
-    visited.insert(cur_node);
+    visited.push_back(cur_node);
 
     // try every edge from current node to continue the path
     int path_count = 0;
     for (auto n : edges[cur_node]) {
-        if (!visited.count(n) || !small_cave(n, node_id_names)) {
-            path_count += count_all_pathes(n, node_ids, edges, node_id_names, visited_twice, visited);
-        }
 
-        // allows to visit one small cave twice (part 2)
-        if (!visited_twice && visited.count(n) && small_cave(n, node_id_names)) {
-            if (n != node_ids["start"] && n != node_ids["end"]) {
-                path_count += count_all_pathes(n, node_ids, edges, node_id_names, true, visited);
-            }
+        // if we can reached the end node, we found a new path
+        if (n == end) {
+            path_count += 1;
+        }
+        else if (!is_small_cave[n] || !is_in(visited, n)) {
+            path_count += count_all_pathes(n, edges, is_small_cave, start, end, visited_twice);
+        }
+        // p2 - if we reach this cur_node is a small cave
+        //      and we will have visited it once before
+        else if (!visited_twice && n != start && n != end) {
+            path_count += count_all_pathes(n, edges, is_small_cave, start, end, true);
         }
     }
 
+    visited.pop_back();
     return path_count;
 }
 
@@ -51,38 +54,35 @@ answer solve_day12(input& in) {
     answer a;
 
     std::map<std::string, int> node_ids;
-    std::map<int, std::set<int>> edges;
-    std::map<int, std::string> node_id_names;
+    std::vector<bool> node_small_cave;
+
+    // for n edges there can be at most 2n nodes
+    std::vector<std::vector<int>> edges(2 * in.size());
 
     int next_id = 0;
+    int start_id = 0;
+    int end_id = 0;
 
     for (auto& l : in) {
-        size_t del_pos = l.find('-');
+        auto nodes = split(l, '-');
 
-        assert(del_pos > 0 && del_pos < l.size());
+        for (const auto& n : nodes) {
+            if (!node_ids.count(n)) {
+                if (n == "start") start_id = next_id;
+                else if (n == "end") end_id = next_id;
 
-        std::string n1 = l.substr(0, del_pos);
-        std::string n2 = l.substr(del_pos+1);
-
-
-        if (!node_ids.count(n1)) {
-            node_id_names.insert(std::make_pair(next_id, n1));
-            node_ids.insert(std::make_pair(n1, next_id++));
-        }
-        if (!node_ids.count(n2)) {
-            node_id_names.insert(std::make_pair(next_id, n2));
-            node_ids.insert(std::make_pair(n2, next_id++));
+                node_small_cave.push_back(n[0] > 96);
+                node_ids.insert(std::make_pair(n, next_id++));
+            }
         }
 
         // bidirected graph, insert edge for both directions
-        edges[node_ids[n1]].insert(node_ids[n2]);
-        edges[node_ids[n2]].insert(node_ids[n1]);
+        edges[node_ids[nodes[0]]].push_back(node_ids[nodes[1]]);
+        edges[node_ids[nodes[1]]].push_back(node_ids[nodes[0]]);
     }
 
-    int start = node_ids["start"];
-
-    a.part1 = std::to_string(count_all_pathes(start, node_ids, edges, node_id_names, true));
-    a.part2 = std::to_string(count_all_pathes(start, node_ids, edges, node_id_names));
+    a.part1 = std::to_string(count_all_pathes(start_id, edges, node_small_cave, start_id, end_id, true));
+    a.part2 = std::to_string(count_all_pathes(start_id, edges, node_small_cave, start_id, end_id));
 
     return a;
 }
