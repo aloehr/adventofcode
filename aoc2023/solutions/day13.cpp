6a4d1659b5e6998a37d019a9849ee4a57625fcf8
Alexander Löhr <a.loehr@ymail.com>
2023-12-13 16:04:08 +0100

    2023 - day 13 - optimization, instead of bruteforcing part 2 we count the differences for a given reflection line and if the difference matches the wanted smudge count, we know that this has to be the wanted reflection line

--- aoc2023/solutions/day13.cpp
+++ aoc2023/solutions/day13.cpp
@@ -3,43 +3,65 @@
 #include <iostream>
 #include <optional>
 #include <string>
+#include <string_view>
 #include <vector>
 
 #include "aoch/AOCSolutionTypes.hpp"
 
-template<typename T>
-bool is_equal(const T& vectorable, size_t l) {
-    size_t i = l;
-    size_t j = l+1;
 
-    while (i < vectorable.size() && j < vectorable.size()) {
-        if (vectorable[i] != vectorable[j]) return false;
-        i--;
-        j++;
+using ImgType = std::vector<std::string_view>;
+
+size_t difference_vertical(const ImgType& img, size_t l, size_t wanted_diff = 0) {
+    size_t diff = 0;
+
+    for (size_t row_idx = 0; row_idx < img.size(); ++row_idx) {
+        size_t i = l;
+        size_t j = l+1;
+
+        while (i < img[row_idx].size() && j < img[row_idx].size()) {
+            if (img[row_idx][i] != img[row_idx][j]) {
+                diff++;
+
+                if (diff > wanted_diff) return diff;
+            }
+
+            i--;
+            j++;
+        }
     }
 
-    return true;
+    return diff;
 }
 
-bool is_reflection_line_vertical(const std::vector<std::string>& img, size_t l) {
-    for (const auto& row : img) {
-        if (!compare(row, l)) return false;
+size_t difference_horizontal(const ImgType& img, size_t l, size_t wanted_diff = 0) {
+    size_t diff = 0;
+
+    for (size_t col_idx = 0; col_idx < img[0].size(); ++col_idx) {
+        size_t i = l;
+        size_t j = l+1;
+
+        while (i < img.size() && j < img.size()) {
+            if (img[i][col_idx] != img[j][col_idx]) {
+                diff++;
+
+                if (diff > wanted_diff) return diff;
+            }
+
+            i--;
+            j++;
+        }
     }
 
-    return true;
-}
-
-bool is_reflection_line_horizontal(const std::vector<std::string>& img, size_t l) {
-    return compare(img, l);
+    return diff;
 }
 
 using ReflectionLine = std::array<size_t, 2>;
 using ReflectionLineOpt = std::optional<ReflectionLine>;
 
-ReflectionLineOpt check_for_reflection(const std::vector<std::string>& img, ReflectionLineOpt ignored_reflection_line = std::nullopt) {
+ReflectionLineOpt check_for_reflection(const ImgType& img, ReflectionLineOpt ignored_reflection_line = std::nullopt, size_t wanted_diff = 0) {
     // check for vertical reflection line
     for (size_t i = 0; i < img[0].size() - 1; ++i) {
-        if (is_reflection_line_vertical(img, i)) {
+        if (difference_vertical(img, i, wanted_diff) == wanted_diff) {
             ReflectionLine ref_line = {0, i+1};
             if (!ignored_reflection_line || *ignored_reflection_line != ref_line) {
                 return ref_line;
@@ -49,7 +71,7 @@ ReflectionLineOpt check_for_reflection(const std::vector<std::string>& img, Refl
 
     // check for horizontal reflection line
     for (size_t i = 0; i < img.size() - 1; ++i) {
-        if (is_reflection_line_horizontal(img, i)) {
+        if (difference_horizontal(img, i, wanted_diff) == wanted_diff) {
             ReflectionLine ref_line = {i+1, 0};
             if (!ignored_reflection_line || *ignored_reflection_line != ref_line) {
                 return ref_line;
@@ -66,39 +88,26 @@ aoch::Result solve_day13(aoch::Input& in) {
     // adding an empty line at the end for easier processing
     in.push_back("");
 
-    std::vector<std::string> img;
+    ImgType img;
     unsigned int sum1 = 0;
     unsigned int sum2 = 0;
 
     for (const auto& l : in) {
-
         if (l != "") {
-            img.push_back(l);
+            img.emplace_back(l);
         } else {
             auto reflection_line = check_for_reflection(img);
+
+            // there should always be exactly one reflection line
+            assert(reflection_line);
             sum1 += (*reflection_line)[0] * 100 + (*reflection_line)[1];
 
-            bool found_ref_line = false;
-
-            // we just alter every pixel and check if it leads to a new reflection line
-            for (size_t i = 0; i < img.size() && !found_ref_line; ++i) {
-                for (size_t j = 0; j < img[i].size(); ++j) {
-                    char tmp = img[i][j];
-                    img[i][j] = img[i][j] == '#' ? '.' : '#';
-
-                    auto reflection_line2 = check_for_reflection(img, reflection_line);
-                    img[i][j] = tmp;
-
-                    if (reflection_line2) {
-                        sum2 += (*reflection_line2)[0] * 100 + (*reflection_line2)[1];
-                        found_ref_line = true;
-                        break;
-                    }
-                }
-            }
+            auto reflection_line2 = check_for_reflection(img, reflection_line, 1);
 
             // there should be exactly one altered pixel that leads to a new reflection line
-            assert(found_ref_line);
+            assert(reflection_line2);
+            sum2 += (*reflection_line2)[0] * 100 + (*reflection_line2)[1];
+
             img.clear();
         }
     }
