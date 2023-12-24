cdd993ce2d5c0d3581b37d5cd1a4e5a8614a9226
Alexander Löhr <a.loehr@ymail.com>
2023-12-24 19:52:51 +0100

    2023 day 23 - optimization, edge pruning, node merging for start and dest and using a vec to keep track of the visited state in the DFS search and alot of refactoring

--- aoc2023/solutions/day23.cpp
+++ aoc2023/solutions/day23.cpp
@@ -2,8 +2,9 @@
 #include<array>
 #include<iostream>
 #include<map>
+#include<queue>
+#include<set>
 #include<string>
-#include<tuple>
 #include<utility>
 #include<vector>
 
@@ -24,20 +25,18 @@ struct Node {
 using GraphT = std::map<PosT, Node*>;
 
 std::vector<PosT> possible_moves(const aoch::Input& map, const PosT pos, const PosT *exclusion = nullptr) {
-    std::vector<PosT> ret;
+    static std::vector<Dir> dirs {
+        Dir::NORTH,
+        Dir::EAST,
+        Dir::SOUTH,
+        Dir::WEST,
+    };
 
     static std::vector<DirT> rel_dirs {
         { 0, -1},
         { 1,  0},
         { 0,  1},
         {-1,  0},
-    };
-
-    static std::vector<Dir> dirs {
-        Dir::NORTH,
-        Dir::EAST,
-        Dir::SOUTH,
-        Dir::WEST,
     };
 
     static std::map<char, Dir> slope_to_dir {
@@ -47,6 +46,7 @@ std::vector<PosT> possible_moves(const aoch::Input& map, const PosT pos, const P
         {'<', Dir::WEST},
     };
 
+    std::vector<PosT> ret;
     char cur_tile = map[pos[1]][pos[0]];
 
     for (const auto dir : dirs) {
@@ -65,7 +65,6 @@ std::vector<PosT> possible_moves(const aoch::Input& map, const PosT pos, const P
 }
 
 std::pair<PosT, unsigned int> walk_path(const aoch::Input& map, PosT cur_pos, PosT last_pos) {
-
     unsigned int cur_steps = 1;
 
     while (true) {
@@ -122,7 +121,7 @@ std::map<PosT, Node*> create_graph(const aoch::Input& map, const std::vector<Pos
 
             if (graph.count(res.first)) {
                 Node *n = graph[res.first];
-                cur.connections.push_back(std::make_pair(n, res.second));
+                cur.connections.push_back({n, res.second});
             }
         }
     }
@@ -130,49 +129,152 @@ std::map<PosT, Node*> create_graph(const aoch::Input& map, const std::vector<Pos
     return graph;
 }
 
-unsigned int dfs(const GraphT g, Node* start, Node* dest) {
-    std::vector<std::tuple<Node*, unsigned int, unsigned int>> q;
-    std::vector<Node*> visited_stack;
-    unsigned int longest_path_steps = 0;
-    unsigned int last_depth = 0;
+void delete_graph(GraphT& g) {
+    for (auto it = g.begin(); it != g.end(); ++it) {
+        delete it->second;
+    }
 
-    q.push_back(std::make_tuple(start, 0, 0));
+    g.clear();
+}
+
+struct QueueItem {
+    Node* n;
+    unsigned int steps;
+    unsigned int depth;
+
+    QueueItem(Node* n, unsigned int steps, unsigned int depth) : n(n), steps(steps), depth(depth) {}
+};
+
+unsigned int dfs(const GraphT g, Node* start, Node* dest) {
+    std::vector<QueueItem> q;
+
+    std::vector<unsigned int > visited_stack;
+    std::vector<unsigned int> visited(g.size(), 0);
+
+    unsigned int longest_path_steps = 0;
+
+    // init state
+    q.emplace_back(start, 0, 0);
 
     while (q.size()) {
-        Node* cur;
-        unsigned int cur_steps;
-        unsigned int cur_depth;
-
-        std::tie(cur, cur_steps, cur_depth) = q.back();
+        QueueItem cur = q.back();
         q.pop_back();
 
-        if (cur_depth && cur_depth <= last_depth) {
-            // remove elements from visited_stack according to the depth diff between
-            // last and current depth.
-            // For a depth diff of N pop the last N+1 elements in visited_stack
-            unsigned int remove_count = last_depth - cur_depth + 1;
-            visited_stack.resize(visited_stack.size() - remove_count);
-        }
-
-        visited_stack.push_back(cur);
-
-        if (cur == dest) {
-            if (cur_steps > longest_path_steps) {
-                longest_path_steps = cur_steps;
-            }
-            last_depth = cur_depth;
+        if (cur.n == dest) {
+            longest_path_steps = std::max(longest_path_steps, cur.steps);
             continue;
         }
 
-        for (const auto& p : cur->connections) {
-            if (std::find(visited_stack.cbegin(), visited_stack.cend(), p.first) != visited_stack.cend()) continue;
-            q.push_back(std::make_tuple(p.first, cur_steps + p.second, cur_depth+1));
+        // pop items from visited_stack and mark them as unvisited until stack_size
+        // matches current depth
+        while (visited_stack.size() != cur.depth) {
+            visited[visited_stack.back()] = 0;
+            visited_stack.pop_back();
         }
 
-        last_depth = cur_depth;
+        visited[cur.n->id] = 1;
+        visited_stack.push_back(cur.n->id);
+
+        for (const auto& p : cur.n->connections) {
+            if (visited[p.first->id]) continue;
+            q.emplace_back(p.first, cur.steps + p.second, cur.depth + 1);
+        }
     }
 
     return longest_path_steps;
+}
+
+void optimize_graph_node_merging(GraphT& g, const std::vector<Node*>& preserve) {
+    bool merged_something;
+
+    do {
+        merged_something = false;
+        for (auto it = g.begin(); it != g.end(); ++it) {
+            Node *cur = it->second;
+            if (cur->connections.size() != 1) continue;
+
+            Node *o = cur->connections[0].first;
+            int weight_between_cur_and_o = cur->connections[0].second;
+
+            // we can't merge cur into o if it is in preserve but we can check if the other side of the
+            // edge is in preserve as well and if that isn't the cause we can merge
+            // the other node into cur instead
+            if (std::find(preserve.cbegin(), preserve.cend(), cur) != preserve.cend()) {
+                if (std::find(preserve.cbegin(), preserve.cend(), o) != preserve.cend()) {
+                    continue;
+                } else {
+                    std::swap(cur, o);
+                }
+            }
+
+            // if we get here we gonna merge cur into o
+
+            auto find_cur = [cur](const auto& a) {
+                return a.first == cur;
+            };
+
+            // remove connection from o to cur
+            o->connections.erase(std::find_if(o->connections.cbegin(), o->connections.cend(), find_cur));
+
+            // add connections from cur towards other nodes to o's connections
+            // and redirect other nodes connections that point towards cur to o
+            for (auto& p : cur->connections) {
+                if (p.first == o) continue;
+
+                // add connection to node o
+                o->connections.push_back({p.first, p.second + weight_between_cur_and_o});
+                auto res = std::find_if(p.first->connections.begin(), p.first->connections.end(), find_cur);
+
+                // redirect other nodes connections from cur to o
+                res->first = o;
+                res->second += weight_between_cur_and_o;
+            }
+
+            // remove cur from graph
+            g.erase(cur->pos);
+            delete cur;
+
+            merged_something = true;
+            break;
+        }
+    } while (merged_something);
+
+    // we have to redo the node id's because we possibly removed nodes
+    // and we want to make sure node id's have no gaps.
+    // This is important for the visited vector in the dfs search.
+    // It assumes that node id < g.size()
+    unsigned int next_free_node_id = 0;
+
+    for (auto& kv : g) {
+        kv.second->id = next_free_node_id++;
+    }
+}
+
+// we remove edges that if they ever would be taken, we couldn't reach the dest anymore.
+void optimize_graph_prune_edges(GraphT& g, Node* start, Node* dest) {
+    std::queue<std::pair<Node*, Node*>> q;
+    std::set<Node*> visited;
+
+    q.push({start, nullptr});
+
+    while (q.size()) {
+        Node *cur, *from;
+        std::tie(cur, from) = q.front();
+        q.pop();
+
+        visited.insert(cur);
+
+        auto it = std::find_if(cur->connections.cbegin(), cur->connections.cend(), [from](const auto& a) { return a.first == from; });
+        if (it != cur->connections.cend()) {
+            cur->connections.erase(it);
+        }
+
+        for (auto& kv : cur->connections) {
+            if (visited.count(kv.first)) continue;
+            if (kv.first->connections.size() > 3) continue;
+            q.push({kv.first, cur});
+        }
+    }
 }
 
 aoch::Result solve_day23(aoch::Input& in) {
@@ -181,11 +283,13 @@ aoch::Result solve_day23(aoch::Input& in) {
     PosT start = {0, 0};
     start[0] = std::find(in[0].cbegin(), in[0].cend(), '.') - in[0].cbegin();
 
-    PosT dest = {0, static_cast<unsigned int>(in.size()-1)};
-    dest[0] = std::find(in[in.size()-1].cbegin(), in[in.size()-1].cend(), '.') - in[in.size()-1].cbegin();
+    PosT dest = {0, static_cast<unsigned int>(in.size() - 1)};
+    dest[0] = std::find(in[in.size() - 1].cbegin(), in[in.size() - 1].cend(), '.') - in[in.size() - 1].cbegin();
 
     auto g = create_graph(in, {start, dest});
+
     a.part1 = std::to_string(dfs(g, g[start], g[dest]));
+    delete_graph(g);
 
 
     // part 2
@@ -197,7 +301,11 @@ aoch::Result solve_day23(aoch::Input& in) {
     }
 
     g = create_graph(in, {start, dest});
+    optimize_graph_node_merging(g, {g[start], g[dest]});
+    optimize_graph_prune_edges(g, g[start], g[dest]);
+
     a.part2 = std::to_string(dfs(g, g[start], g[dest]));
+    delete_graph(g);
 
     return a;
 }
