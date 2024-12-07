911f05533f48d201ab254933d86a45b77c50f30d
Alexander Löhr <a.loehr@ymail.com>
2024-12-07 17:25:48 +0100

    2024 day 07 - optimization: instead of going from left to right and do the operations until the goal value is reached, now it goes from right to left and undo's the operations on the goal value until the goal value reaches 0. Because of this we can skip opearations way more often (if modulo is not 0 for division or if the last part of the goal value doesn't match the cur value, which means concatenation is not valid). Also improved the performance of parsing the input significantly.

--- aoc2024/solutions/day07.cpp
+++ aoc2024/solutions/day07.cpp
@@ -1,76 +1,103 @@
 #include <string>
 #include <utility>
+#include <iostream>
 #include <vector>
 
 #include <aoch/AOCSolutionTypes.hpp>
-#include <aoch/string.hpp>
 
 
-bool is_possible(const std::pair<long, std::vector<int>>& eq, const size_t i = 0, const long cur_val = 0) {
-    if (i == eq.second.size()) return cur_val == eq.first;
+bool is_possible(const std::vector<int>& vals, size_t i, const long long cur_result) {
+    if (i >= vals.size()) return cur_result == 0;
 
-    if (cur_val > eq.first) return false;
-
-    if (i == 0) {
-        return is_possible(eq, 1, eq.second[0]);
+    if (cur_result % vals[i] == 0 && is_possible(vals, i - 1, cur_result / vals[i])) {
+        return true;
     }
 
-    return is_possible(eq, i + 1, cur_val + eq.second[i]) ||
-           is_possible(eq, i + 1, cur_val * eq.second[i]);
+    const long long tmp = cur_result - vals[i];
+    if (tmp >= 0 && is_possible(vals, i - 1, tmp)) {
+        return true;
+    }
+
+    return false;
 }
 
-int digits_shift_multi(int val) {
-    int ret = 1;
+bool is_possible2(const std::vector<int>& vals, size_t i, const long long cur_result) {
+    if (i >= vals.size()) return cur_result == 0;
 
-    while (val) {
-        ret *= 10;
-        val /= 10;
+
+    if (cur_result >= vals[i]) {
+        int val = vals[i];
+        long long next_result = cur_result;
+
+        while (val && (val % 10) == (next_result % 10)) {
+            val /= 10;
+            next_result /= 10;
+        }
+
+        if (val == 0 && is_possible2(vals, i - 1, next_result)) {
+            return true;
+        }
+    }
+
+    if (cur_result % vals[i] == 0) {
+        if (is_possible2(vals, i - 1, cur_result / vals[i])) return true;
+    }
+
+    const long long tmp = cur_result - vals[i];
+    if (tmp >= 0 && is_possible2(vals, i - 1, tmp)) {
+        return true;
+    }
+
+    return false;
+}
+
+long long parse_long(const std::string& s, const size_t start, const size_t end) {
+    long long ret = 0;
+
+    for (size_t i = start; i < end; ++i) {
+        ret = ret * 10 + (s[i] & 0xF);
     }
 
     return ret;
 }
 
-bool is_possible2(const std::pair<long, std::vector<int>>& eq, size_t i = 0, long cur_val = 0) {
-    if (cur_val > eq.first) return false;
-    if (i == eq.second.size()) return cur_val == eq.first;
-
-    if (i == 0) {
-        return is_possible2(eq, 1, eq.second[0]);
-    }
-
-    return is_possible2(eq, i + 1, cur_val + eq.second[i]) ||
-           is_possible2(eq, i + 1, cur_val * eq.second[i]) ||
-           is_possible2(eq, i + 1, cur_val * digits_shift_multi(eq.second[i]) + eq.second[i]);
-}
-
 aoch::Result solve_day07(aoch::Input& in) {
     aoch::Result a;
 
-    std::vector<std::pair<long, std::vector<int>>> equations;
+    std::vector<std::pair<long long, std::vector<int>>> equations;
     equations.reserve(in.size());
 
     for (const auto& line : in) {
-        const auto tokens1 = aoch::split(line, ':');
-        const auto tokens2 = aoch::split(tokens1[1]);
+        std::vector<int> vals;
+        vals.reserve(12);
 
-        std::pair<long, std::vector<int>> cur_eq;
-        cur_eq.first = std::stol(tokens1[0]);
-        cur_eq.second.reserve(tokens1[2].size());
+        size_t cur_num_start = 0;
+        size_t cur_num_end = line.find_first_of(':');
 
-        for (const auto& t : tokens2) {
-            cur_eq.second.push_back(std::stoi(t));
+        const long long result = parse_long(line, cur_num_start, cur_num_end);
+
+        cur_num_start = cur_num_end + 2;
+
+        while (cur_num_start < line.size()) {
+            cur_num_end = line.find_first_of(' ', cur_num_start);
+            if (cur_num_end == std::string::npos) cur_num_end = line.size();
+
+            vals.push_back(parse_long(line, cur_num_start, cur_num_end));
+
+            cur_num_start = cur_num_end + 1;
         }
 
-        equations.push_back(std::move(cur_eq));
+        equations.emplace_back(result, std::move(vals));
     }
 
-    long sum  = 0;
-    long sum2 = 0;
+    long long sum  = 0;
+    long long sum2 = 0;
+
     for (const auto& eq : equations) {
-        if (is_possible(eq)) {
+        if (is_possible(eq.second, eq.second.size() - 1, eq.first)) {
             sum  += eq.first;
             sum2 += eq.first;
-        } else if (is_possible2(eq)) {
+        } else if (is_possible2(eq.second, eq.second.size() - 1, eq.first)) {
             sum2 += eq.first;
         }
     }
