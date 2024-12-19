4c60db18e35fd00c2b197fdc3f3d7f5cc98e68b3
Alexander Löhr <a.loehr@ymail.com>
2024-12-20 00:09:22 +0100

    2024 day 19 - optimization: using a trie for prefix matching

--- aoc2024/solutions/day19.cpp
+++ aoc2024/solutions/day19.cpp
@@ -1,4 +1,3 @@
-#include <algorithm>
 #include <array>
 #include <iostream>
 #include <string>
@@ -9,23 +8,58 @@
 #include <aoch/AOCSolutionTypes.hpp>
 
 
-using jump_table_t = std::array<std::array<size_t, 2>, 128>;
+struct TrieNode {
+    std::array<TrieNode*, 26> children{};
+    bool is_end = false;
+};
 
-bool starts_with(const std::string_view s, const std::string_view cmp) {
-    if (cmp.size() > (s.size())) return false;
+struct PatternTrie {
+    TrieNode root;
 
-    for (size_t i = 0; i < cmp.size(); ++i) {
-        if (s[i] != cmp[i]) return false;
+    void add_pattern(const std::string_view p) {
+        TrieNode* cur = &root;
+
+        for (auto c : p) {
+            const size_t idx = c - 'a';
+
+            if (cur->children[idx] == nullptr) {
+                cur->children[idx] = new TrieNode;
+            }
+
+            cur = cur->children[idx];
+        }
+
+        cur->is_end = true;
     }
 
-    return true;
-}
+    std::vector<size_t> prefix_matches(const std::string_view s) const {
+        std::vector<size_t> ret;
+
+        size_t depth = 0;
+        const TrieNode* cur = &root;
+
+        for (auto c : s) {
+            const size_t idx = c - 'a';
+
+            if (cur->children[idx] == nullptr) break;
+
+            depth++;
+
+            cur = cur->children[idx];
+
+            if (cur->is_end) {
+                ret.push_back(depth);
+            }
+        }
+
+        return ret;
+    }
+};
 
 long long design_possible(
     const std::string_view design,
-    const std::vector<std::string_view>& patterns,
-    std::unordered_map<std::string_view, long long>& memo,
-    const jump_table_t& jump_table
+    const PatternTrie& pt,
+    std::unordered_map<std::string_view, long long>& memo
 ) {
     if (design.empty()) return 1;
 
@@ -35,69 +69,55 @@ long long design_possible(
 
     long long total = 0;
 
-    for (auto [i, end] = jump_table[design[0]]; i < end; ++i) {
-        if (starts_with(design, patterns[i])) {
-            total += design_possible(design.substr(patterns[i].size()), patterns, memo, jump_table);
-        }
+    for (size_t length : pt.prefix_matches(design)) {
+        total += design_possible(design.substr(length), pt, memo);
     }
 
     memo[design] = total;
     return total;
 }
 
-std::vector<std::string_view> parse_patterns(std::string_view s) {
-    std::vector<std::string_view> ret;
+PatternTrie create_pattern_trie(std::string_view s) {
+    PatternTrie pt;
 
     size_t start = 0;
     size_t end;
 
     while ((end = s.find_first_of(',', start)) != std::string::npos) {
-        ret.push_back(s.substr(start, end - start));
+        pt.add_pattern(s.substr(start, end - start));
 
+        auto x = pt.prefix_matches(s.substr(start, end - start));
+        if (x.size() == 0) std::cout << s.substr(start, end - start) << std::endl;
         // skipping comma and whie space
         start = end + 2;
     }
 
     // add the last pattern
-    ret.push_back(s.substr(start));
+    pt.add_pattern(s.substr(start));
 
-    return ret;
+    return pt;
 }
 
 aoch::Result solve_day19(aoch::Input& in) {
     aoch::Result a;
 
-    std::vector<std::string_view> patterns = parse_patterns(in[0]);
+    PatternTrie pt = create_pattern_trie(in[0]);
 
-    std::sort(patterns.begin(), patterns.end());
-
-    jump_table_t jump_table{};
-
-    for (size_t i = 0; i < patterns.size(); ++i) {
-        const char c = patterns[i][0];
-        if (jump_table[c][1] == 0) {
-            jump_table[c] = {i, patterns.size()};
-
-            if (i > 0) {
-                jump_table[patterns[i-1][0]][1] = i;
-            }
-        }
-    }
-
-    std::vector<std::string> designs;
+    std::vector<std::string_view> designs;
     designs.reserve(in.size() - 2);
 
     for (size_t i = 2; i < in.size(); ++i) {
-        designs.push_back(std::move(in[i]));
+        designs.push_back(in[i]);
     }
 
     int count_possible = 0;
     long long total_ways = 0;
 
-    std::unordered_map<std::string_view, long long> memo;
+    std::unordered_map<std::string_view, long long> memo(100);
 
-    for (const auto& d : designs) {
-        long long possible_ways = design_possible(d, patterns, memo, jump_table);
+    for (const auto d : designs) {
+        memo.clear();
+        long long possible_ways = design_possible(d, pt, memo);
 
         count_possible += possible_ways != 0;
         total_ways += possible_ways;
